de02d180326bf31386ce9be503a79d41487e48ce
MR0UBLO <nyeripublo@gmail.com>
2026-08-03 14:24:32 +0300

    Update App.tsx

--- artifacts/big-profits/src/App.tsx
+++ artifacts/big-profits/src/App.tsx
@@ -1,3 +1,4 @@
+import Wallet from "@/pages/wallet";
 import { Switch, Route, useLocation } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
