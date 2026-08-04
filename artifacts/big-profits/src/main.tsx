f32524260f9c80988ca71ce61d2344bcdb17a8b5
MR0UBLO <nyeripublo@gmail.com>
2026-08-04 16:13:57 +0300

    Update main.tsx

--- artifacts/big-profits/src/main.tsx
+++ artifacts/big-profits/src/main.tsx
@@ -2,8 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { setBaseUrl } from "@workspace/api-client-react";
+import { getSocket } from "./lib/socket";
 
 setBaseUrl(import.meta.env.VITE_API_URL);
+
+// Connect socket as soon as the app starts
+getSocket();
 
 createRoot(document.getElementById("root")!).render(
   <App />
